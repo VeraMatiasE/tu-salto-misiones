c6fb09a5cc37e33a24c5c4826784480294fce6be
Joassanchez <Joassanchez03@gmail.com>
2025-05-16 16:54:23 -0300

    feat: integración de autenticación con Supabase en Next.js

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,28 @@
+import { logOut } from "@/actions/auth";
+import { createSupabaseClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const Profile = async () => {
+  const supabase = await createSupabaseClient();
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  if (error) redirect("/error");
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <form className="flex flex-col gap-5 w-1/4">
+        <h1 className="text-3xl font-bold">Profile: {user?.email}</h1>
+        <button
+          formAction={logOut}
+          className="p-2 bg-red-800 h-10 pointer hover:bg-red-700 transition-all rounded-sm flex items-center justify-center text-white font-bold w-full"
+        >
+          Log out
+        </button>
+      </form>
+    </main>
+  );
+};
+
+export default Profile;
