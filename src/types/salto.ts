32e07feb7fd3568c28053384941dba7b5bf41921
Vera Matías <89822359+VeraMatiasE@users.noreply.github.com>
2025-05-31 18:31:26 -0300

    Added dashboard of new salto

--- src/types/salto.ts
+++ src/types/salto.ts
@@ -6,3 +6,16 @@ export type Salto = {
   costo: number
   dificultad: "baja" | "media" | "alta" | "extrema"
 }
+
+export type SaltoFormProps = {
+  initialData?: {
+    nombre: string
+    ubicacion: string
+    url_map: string
+    costo_entrada: number
+    infraestructura: [string]
+    biodiversidad: string
+    dificultad: 'baja' | 'media' | 'alta' | 'extrema' | undefined
+    servicios: string[] | undefined
+  }
+}
