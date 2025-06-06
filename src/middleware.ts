4e6f32aebf814935e0a488c1473d9caea4482a4c
Vera Matías <89822359+VeraMatiasE@users.noreply.github.com>
2025-06-06 01:10:57 -0300

    Added admin protection route

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,29 @@ export async function middleware(request: NextRequest, response: NextResponse) {
     const protectedRoutesList = ["/profile"],
         authRoutesList = ["/", "/log-in", "/sing-up"];
     const currentPath = new URL(request.url).pathname;
+    const adminRoutesList = ["/dashboard"];
     const {
-        data: { user },
+        user, supabase
     } = await getUser(request, response);
     if (protectedRoutesList.includes(currentPath) && !user) {
         return NextResponse.redirect(new URL("/log-in", request.url));
     }
     if (authRoutesList.includes(currentPath) && user) {
         return NextResponse.redirect(new URL("/profile", request.url));
+    }
+    if (adminRoutesList.some(route => currentPath.startsWith(route))) {
+        if (!user) {
+            return NextResponse.redirect(new URL("/log-in", request.url));
+        }
+        const { data: userProfile, error } = await supabase
+            .from("usuarios")
+            .select("rol")
+            .eq("uid_usuario", user.id)
+            .single();
+
+        if (error || !userProfile || !(userProfile.rol) ) {
+            return NextResponse.redirect(new URL("/unauthorized", request.url));
+        }
     }
     await updateSession(request);
 }
